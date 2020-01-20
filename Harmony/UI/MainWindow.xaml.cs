d1e76e9c4bcb9a0a2f2ba04e6045f29722318033
Flo56958 <florian.gutsche@fau.de>
2020-01-20 08:41:53 +0100

    Adding Overlap Detection

--- Harmony/UI/MainWindow.xaml.cs
+++ Harmony/UI/MainWindow.xaml.cs
@@ -39,8 +39,7 @@ namespace Harmony {
                 }
 
                 Model.NotStarted = false;
-            }
-            else {
+            } else {
                 if (NetworkCommunicator.Instance != null) {
                     NetworkCommunicator.Instance.Close();
                     NetworkCommunicator.Instance = null;
@@ -58,7 +57,7 @@ namespace Harmony {
         }
 
         public static void Log(string message, bool error) {
-            var err = error ? "[ERROR] " : "[INFO] "; 
+            var err = error ? "[ERROR] " : "[INFO] ";
             _window.Dispatcher?.Invoke(() => {
                 _window.DebugTextBox.AppendText(err + message + "\n");
             });
@@ -71,10 +70,10 @@ namespace Harmony {
                 if (!(obj is Canvas)) continue;
                 var c = (Canvas)obj;
                 if (!c.Background.Equals(ThemeManager.GetResourceFromAppStyle(this, "MahApps.Brushes.Accent"))) continue;
-                var i = int.Parse(((TextBlock) c.Children[0]).Text);
+                var i = int.Parse(((TextBlock)c.Children[0]).Text);
                 DisplayManager.Displays[i].Location = new System.Drawing.Point {
-                    X = (int) ((Canvas.GetLeft(c) + (main.Screen.Width / shrink) / 2) * shrink),
-                    Y = (int) ((Canvas.GetTop(c) + (main.Screen.Height / shrink) / 2) * shrink)
+                    X = (int)((Canvas.GetLeft(c) + (main.Screen.Width / shrink) / 2) * shrink),
+                    Y = (int)((Canvas.GetTop(c) + (main.Screen.Height / shrink) / 2) * shrink)
                 };
             }
             NetworkCommunicator.Instance?.SendAsync(new HarmonyPacket {
@@ -103,7 +102,7 @@ namespace Harmony {
                     Height = dis.Screen.Height / shrink,
                     MaxWidth = dis.Screen.Width / shrink,
                     MaxHeight = dis.Screen.Height / shrink,
-                    Background = (Brush) (dis.OwnDisplay ? ThemeManager.GetResourceFromAppStyle(this, "MahApps.Brushes.Accent2") : ThemeManager.GetResourceFromAppStyle(this, "MahApps.Brushes.Accent")),
+                    Background = (Brush)(dis.OwnDisplay ? ThemeManager.GetResourceFromAppStyle(this, "MahApps.Brushes.Accent2") : ThemeManager.GetResourceFromAppStyle(this, "MahApps.Brushes.Accent")),
                     Opacity = 100
                 };
 
@@ -136,50 +135,139 @@ namespace Harmony {
 
                 if (Model.IsMaster && !dis.OwnDisplay) {
                     canv.MouseLeftButtonDown += (s, eArgs) => {
-                        var c = ((Canvas)s);
-                        c.Opacity = 20;
-                        var deltaLeft = Canvas.GetLeft(c);
-                        var deltaTop = Canvas.GetTop(c);
-                        Canvas.SetLeft(c, eArgs.GetPosition(DisplayCanvas).X - c.Width / 2);
-                        Canvas.SetTop(c, eArgs.GetPosition(DisplayCanvas).Y - c.Height / 2);
-                        deltaLeft = Canvas.GetLeft(c) - deltaLeft;
-                        deltaTop = Canvas.GetTop(c) - deltaTop;
+                        MoveAllScreens((Canvas)s, eArgs.GetPosition(DisplayCanvas));
 
-                        foreach (var can in DisplayCanvas.Children) {
-                            if (can.Equals(c)) continue;
-                            if (!(can is Canvas)) continue;
-                            var canvas = (Canvas)can;
-                            if (!canvas.Background.Equals(canv.Background)) continue;
-                            Canvas.SetLeft(canvas, Canvas.GetLeft(canvas) + deltaLeft);
-                            Canvas.SetTop(canvas, Canvas.GetTop(canvas) + deltaTop);
-                        }
                     };
                     canv.MouseLeftButtonUp += (s, eArgs) => {
-                        ((Canvas)s).Opacity = 100;
+                        var c = (Canvas)s;
                         //TODO: Snap to other Screens
+                        var changed = false;
+                        do {
+                            changed = false;
+                            foreach (var obj in DisplayCanvas.Children) {
+                                if (!(obj is Canvas)) continue;
+                                if (c == obj) continue;
+                                var o = (Canvas)obj;
+                                if (o.Background.Equals(c.Background)) continue;
+                                if (!Intersects(c, o)) continue;
+                                changed = true;
+
+                                var lt = new Point(Canvas.GetLeft(c), Canvas.GetTop(c));
+                                var lb = new Point(Canvas.GetLeft(c), Canvas.GetTop(c) + c.Height);
+                                var rt = new Point(Canvas.GetLeft(c) + c.Width, Canvas.GetTop(c));
+                                var rb = new Point(Canvas.GetLeft(c) + c.Width, Canvas.GetTop(c) + c.Height);
+
+                                double dx = 0;
+                                double dy = 0;
+                                if (IsPointInCanvas(lt, o)) {
+                                    var ddx = Canvas.GetLeft(o) - lt.X;
+                                    var ddy = Canvas.GetTop(o) - lt.Y;
+                                    dx = Math.Max(dx, Math.Abs(ddx));
+                                    if (dx == Math.Abs(ddx)) {
+                                        dx = ddx;
+                                    }
+                                    dy = Math.Max(dy, Math.Abs(ddy));
+                                    if (dy == Math.Abs(ddy)) {
+                                        dy = ddy;
+                                    }
+                                }
+
+                                if (IsPointInCanvas(lb, o)) {
+                                    var ddx = Canvas.GetLeft(o) - lb.X;
+                                    var ddy = Canvas.GetTop(o) + o.Height - lb.Y;
+                                    dx = Math.Max(dx, Math.Abs(ddx));
+                                    if (dx == Math.Abs(ddx)) {
+                                        dx = ddx;
+                                    }
+                                    dy = Math.Max(dy, Math.Abs(ddy));
+                                    if (dy == Math.Abs(ddy)) {
+                                        dy = ddy;
+                                    }
+                                }
+
+                                if (IsPointInCanvas(rt, o)) {
+                                    var ddx = Canvas.GetLeft(o) + o.Width - rt.X;
+                                    var ddy = Canvas.GetTop(o) - rt.Y;
+                                    dx = Math.Max(dx, Math.Abs(ddx));
+                                    if (dx == Math.Abs(ddx)) {
+                                        dx = ddx;
+                                    }
+                                    dy = Math.Max(dy, Math.Abs(ddy));
+                                    if (dy == Math.Abs(ddy)) {
+                                        dy = ddy;
+                                    }
+                                }
+
+                                if (IsPointInCanvas(rb, o)) {
+                                    var ddx = Canvas.GetLeft(o) + o.Width - rb.X;
+                                    var ddy = Canvas.GetTop(o) + o.Height - rb.Y;
+                                    dx = Math.Max(dx, Math.Abs(ddx));
+                                    if (dx == Math.Abs(ddx)) {
+                                        dx = ddx;
+                                    }
+                                    dy = Math.Max(dy, Math.Abs(ddy));
+                                    if (dy == Math.Abs(ddy)) {
+                                        dy = ddy;
+                                    }
+                                }
+
+                                dx -= Canvas.GetLeft(o);
+                                dy -= Canvas.GetTop(o);
+
+                                if (Math.Abs(dx) >= Math.Abs(dy)) {
+                                    Canvas.SetLeft(c, Canvas.GetLeft(c) - dx);
+                                }
+                                else {
+                                    Canvas.SetTop(c, Canvas.GetTop(c) - dy);
+                                }
+
+                                MoveAllScreens(c, new Point(Canvas.GetLeft(c), Canvas.GetTop(c)));
+                            }
+                        } while (changed);
                     };
                     canv.MouseMove += (s, eArgs) => {
-                        var c = ((Canvas)s);
-                        if (eArgs.LeftButton != System.Windows.Input.MouseButtonState.Pressed ||
-                            !(Math.Abs(c.Opacity - 20) < 0.1)) return;
-                        var deltaLeft = Canvas.GetLeft(c);
-                        var deltaTop = Canvas.GetTop(c);
-                        Canvas.SetLeft(c, eArgs.GetPosition(DisplayCanvas).X - c.Width / 2);
-                        Canvas.SetTop(c, eArgs.GetPosition(DisplayCanvas).Y - c.Height / 2);
-                        deltaLeft = Canvas.GetLeft(c) - deltaLeft;
-                        deltaTop = Canvas.GetTop(c) - deltaTop;
-
-                        foreach (var can in DisplayCanvas.Children) {
-                            if (can.Equals(c)) continue;
-                            if (!(can is Canvas)) continue;
-                            var canvas = (Canvas)can;
-                            if (!canvas.Background.Equals(canv.Background)) continue;
-                            Canvas.SetLeft(canvas, Canvas.GetLeft(canvas) + deltaLeft);
-                            Canvas.SetTop(canvas, Canvas.GetTop(canvas) + deltaTop);
-                        }
+                        var c = (Canvas)s;
+                        if (eArgs.LeftButton != System.Windows.Input.MouseButtonState.Pressed) return;
+                        MoveAllScreens(c, eArgs.GetPosition(DisplayCanvas));
                     };
                 }
                 DisplayCanvas.Children.Add(canv);
+            }
+        }
+
+        private static bool IsPointInCanvas(Point p, Canvas c) {
+            return p.X > Canvas.GetLeft(c) && p.X < Canvas.GetLeft(c) + c.Width
+                                           && p.Y > Canvas.GetTop(c) && p.Y < Canvas.GetTop(c) + c.Height;
+        }
+
+        private static bool Intersects(Canvas c1, Canvas c2) {
+            var lt = new Point(Canvas.GetLeft(c1), Canvas.GetTop(c1));
+            var lb = new Point(Canvas.GetLeft(c1), Canvas.GetTop(c1) + c1.Height);
+            var rt = new Point(Canvas.GetLeft(c1) + c1.Width, Canvas.GetTop(c1));
+            var rb = new Point(Canvas.GetLeft(c1) + c1.Width, Canvas.GetTop(c1) + c1.Height);
+            return IsPointInCanvas(lt, c2) || IsPointInCanvas(lb, c2) || IsPointInCanvas(rt, c2) ||
+                   IsPointInCanvas(rb, c2);
+            return Canvas.GetLeft(c1) < Canvas.GetLeft(c2) + c2.Width
+                   && Canvas.GetLeft(c1) + c1.Width > Canvas.GetLeft(c2)
+                   && Canvas.GetTop(c1) > Canvas.GetTop(c2) + c2.Height
+                   && Canvas.GetTop(c1) + c1.Height < Canvas.GetTop(c2);
+        }
+
+        private void MoveAllScreens(Canvas c, Point p) {
+            var deltaLeft = Canvas.GetLeft(c);
+            var deltaTop = Canvas.GetTop(c);
+            Canvas.SetLeft(c, p.X - c.Width / 2);
+            Canvas.SetTop(c, p.Y - c.Height / 2);
+            deltaLeft = Canvas.GetLeft(c) - deltaLeft;
+            deltaTop = Canvas.GetTop(c) - deltaTop;
+
+            foreach (var can in DisplayCanvas.Children) {
+                if (can.Equals(c)) continue;
+                if (!(can is Canvas)) continue;
+                var canvas = (Canvas)can;
+                if (!canvas.Background.Equals(c.Background)) continue;
+                Canvas.SetLeft(canvas, Canvas.GetLeft(canvas) + deltaLeft);
+                Canvas.SetTop(canvas, Canvas.GetTop(canvas) + deltaTop);
             }
         }
 
